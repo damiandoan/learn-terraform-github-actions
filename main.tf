18ceb98b7aeb9d20213e1f5debd4b0b0e5ee234c
katzenundundle <nghiadoan040501@gmail.com>
2023-08-20 22:36:41 +0700

    change my name

--- main.tf
+++ main.tf
@@ -54,7 +54,7 @@ resource "aws_instance" "web" {
               apt-get update
               apt-get install -y apache2
               sed -i -e 's/80/8080/' /etc/apache2/ports.conf
-              echo "Hello World" > /var/www/html/index.html
+              echo "Hello World, Im Nghia" > /var/www/html/index.html
               systemctl restart apache2
               EOF
 }
